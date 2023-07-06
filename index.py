de7a2c034b866d804baf97e8377408108ad2a9a9
NehaS101 <solankineha297@gmail.com>
2023-07-07 00:24:43 +0530

    updated code

--- index.py
+++ index.py
@@ -1,36 +1,47 @@
 #importing random modules
 import random
 
-#your choice from input
-user_choice = input("please enter you choice(rock,paper,scissor): ")
+res=""
+def play(user_choice):
 
 #computer's choice
-choices=["rock", "paper", "scissor"]
-computer_choice = random.choice(choices)
+ choices=["rock", "paper", "scissor"]
+ computer_choice = random.choice(choices)
 
 #declaring winner according to choice
-if user_choice == computer_choice:
+ if user_choice == computer_choice:
     res = "match draw"
-elif (user_choice == "rock" or computer_choice == "scissor") or (user_choice == "paper" or computer_choice == "rock") or (user_choice == "scissor" or computer_choice == "paper"):  
+ elif (user_choice == "rock" or computer_choice == "scissor") or (user_choice == "paper" or computer_choice == "rock") or (user_choice == "scissor" or computer_choice == "paper"):  
     res = "you win, congratulations!"
-else:
+ else:
     res = "computer wins, better luck next time"
 
-print(f"your choice : {user_choice}")
-print(f"computer wins : {computer_choice}")
-print(res)
+ print(f"your choice : {user_choice}")
+ print(f"computer choice : {computer_choice}")
+ print(res)
+
 
 
 #score of winners
-user_win=0
-comp_win=0
-draw_win=0
+ user_win=0
+ comp_win=0
+ draw_win=0
 
-if res== "match draw":
+ if res== "match draw":
     draw_win += 1
-elif res == "you win, congratulations!":
+ elif res == "you win, congratulations!":
     user_win += 1
-else:
+ else:
     comp_win += 1
 
-print(f"Score: User-{user_win}, Computer-{comp_win}, Draw-{draw_win}")            
+ print(f"Score: User-{user_win}, Computer-{comp_win}, Draw-{draw_win}")     
+
+#your choice from input
+user_choice = input("please enter you choice(rock,paper,scissor): ")
+
+#quitting game
+while user_choice!="quit":
+    play(user_choice)
+    user_choice=input("please enter you choice(rock,paper,scissor,quit):")
+
+print("thank you for playing")    
